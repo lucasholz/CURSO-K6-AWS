ff4a9da5a8014a209cc301a3b0022bb01d7f47e5
Lucas Holz <matteholz@gmail.com>
2024-07-23 14:05:19 -0300

    ci: add report html

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,7 @@
 import http from 'k6/http'
 import { check } from 'k6'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 
 
 export const options = {
@@ -18,4 +20,10 @@ export default function() {
   check(res, {
     'status code 200': (r) => r.status === 200
   })
+}
+
+export function handleSummary(data) {
+  return {
+    "index.html": htmlReport(data),
+  };
 }
